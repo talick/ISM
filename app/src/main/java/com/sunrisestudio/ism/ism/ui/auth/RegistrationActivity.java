fd716d17def9dd892ec1c61f3bb85cb24d673202
Talgat <talgat.asanbaev@gmail.com>
2018-09-14 13:22:19 +0600

    "init"

---
+++ app/src/main/java/com/sunrisestudio/ism/ism/ui/auth/RegistrationActivity.java
@@ -0,0 +1,34 @@
+package com.sunrisestudio.ism.ism.ui.auth;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+
+import com.sunrisestudio.ism.ism.R;
+public class RegistrationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(Color.WHITE);
+
+            View view = this.getCurrentFocus();
+            if (view != null) {
+                InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+            }
+        }
+    }
+
+    public void onClickConfirm(View view) {
+     //   startActivity(new Intent(this, MainActivity.class));
+       // finish();
+    }
+}
---
+++ app/src/main/java/com/sunrisestudio/ism/ism/ui/splash/SplashScreenActivity.java
@@ -0,0 +1,30 @@
+package com.sunrisestudio.ism.ism.ui.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+import com.sunrisestudio.ism.ism.R;
+import com.sunrisestudio.ism.ism.ui.auth.RegistrationActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    private final int SPLASH_DISPLAY_LENGTH = 1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        new Handler().postDelayed(new Runnable(){
+            @Override
+            public void run() {
+                /* Create an Intent that will start the Menu-Activity. */
+
+                startActivity(new Intent(SplashScreenActivity.this, RegistrationActivity.class));
+                finish();
+            }
+        }, SPLASH_DISPLAY_LENGTH);
+    }
+}
